acb4fcd70e1261bc4227e882a31467f665f4db81
Leodolter Maximilian <Maximilian.Leodolter@ait.ac.at>
2019-09-26 17:39:57 +0200

    R

--- R/test.R
+++
@@ -1,2 +0,0 @@
-#How TO connect a R Package-Project with local and online Github
-#browseURL("https://pjnewcombe.wordpress.com/2014/05/31/using-rstudio-to-create-and-maintain-an-r-package-on-github/")
